59c58cb55d4c33fe56a8bb97540a48a025901b15
kozharsky <dmitriy.kozharsky@gmail.com>
2016-09-21 16:27:44 +0300

    steps

---
+++ start.sh
@@ -0,0 +1,45 @@
+#!/bin/bash 
+
+#add in .bashrc
+
+#if [ -d $HOME/.rbenv ]; then
+#export PATH="$HOME/.rbenv/bin:$PATH"
+#eval "$(rbenv init -)"
+#fi
+
+#apt-get -y install git-core curl
+
+#curl -L raw.github.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash
+
+#. ~/.bashrc
+
+#sudo apt-get install autoconf bison build-essential libssl-dev libyaml-dev libreadline6 libreadline6-dev zlib1g zlib1g-dev
+
+#sudo chown -R vagrant:vagrant /home/vagrant/.rbenv/
+
+#rbenv install 2.3.1
+
+#rbenv global 2.3.1
+
+#gem install bundler
+
+#gem install rails -v 5.0.0
+
+#mkdir -p /home/vagrant/sinc/web
+
+#cd /home/vagrant/sinc/web
+
+#wget https://github.com/drhenner/ror_ecommerce/archive/master.zip
+
+#sudo apt-get install unzip
+
+#unzip /home/vagrant/sinc/web/master.zip
+
+#cd /home/vagrant/sinc/web/ror_ecommerce-master
+
+#bundle install
+
+#Permission denied (publickey). не может с Гитом подружиться
+
+
+
